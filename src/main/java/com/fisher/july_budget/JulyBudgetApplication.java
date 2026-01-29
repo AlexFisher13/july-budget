50310c1cc6b97d160d8968a182505a9af2a907b5
fisher <fisheralexandr@gmail.com>
2026-01-29 22:39:29 +0300

    git init

---
+++ src/main/java/com/fisher/july_budget/JulyBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.fisher.july_budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulyBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fisher/july_budget/JulyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisher.july_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
